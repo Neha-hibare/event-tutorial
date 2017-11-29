5039dd07bb9880966ed069a01c2802dddc3d593f
Neha-Hibare <nehahibare92@gmail.com>
2017-11-29 12:07:48 +0530

    First commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,47 @@
+import { Component, NgZone } from '@angular/core';
+import { Platform } from 'ionic-angular';
+import { StatusBar } from '@ionic-native/status-bar';
+import { SplashScreen } from '@ionic-native/splash-screen';
+
+import { HomePage } from '../pages/home/home';
+
+import firebase from 'firebase';
+
+@Component({
+  templateUrl: 'app.html'
+})
+export class MyApp {
+  rootPage: any;
+  zone: NgZone;
+
+  constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen) {
+    this.zone = new NgZone({});
+    
+    firebase.initializeApp({
+      apiKey: "AIzaSyBwEUe6x_w_yLFrr--xYLQJLxRT2Rc8vtY",
+      authDomain: "ionic-firebase-auth-9f555.firebaseapp.com",
+      databaseURL: "https://ionic-firebase-auth-9f555.firebaseio.com",
+      storageBucket: "ionic-firebase-auth-9f555.appspot.com",
+      messagingSenderId: "904481277327"
+    });
+
+    const unsubscribe = firebase.auth().onAuthStateChanged((user) => {
+      this.zone.run( () => {
+        if (!user) {
+          this.rootPage = 'login';
+          unsubscribe();
+        } else { 
+          this.rootPage = HomePage;
+          unsubscribe();
+        }
+      });     
+    });
+
+    platform.ready().then(() => {
+      // Okay, so the platform is ready and our plugins are available.
+      // Here you can do any higher level native things you might need.
+      statusBar.styleDefault();
+      splashScreen.hide();
+    });
+  }
+}
---
+++ src/providers/event/event.ts
@@ -0,0 +1,78 @@
+import { Injectable } from '@angular/core';
+import firebase from 'firebase';
+
+@Injectable()
+export class EventProvider {
+
+  constructor() {}
+
+  getEventList(): Promise<any> {
+    return new Promise( (resolve, reject) => {
+      firebase.database().ref(`userProfile/${firebase.auth().currentUser.uid}/eventList`)
+      .on('value', snapshot => {
+        let rawList = [];
+        snapshot.forEach( snap => {
+          rawList.push({
+            id: snap.key,
+            name: snap.val().name,
+            price: snap.val().price,
+            date: snap.val().date,
+          });
+        return false
+        });
+        resolve(rawList);
+      });
+    });
+
+  }
+
+  getEventDetail(eventId): Promise<any> {
+    return new Promise( (resolve, reject) => {
+      firebase.database().ref(`userProfile/${firebase.auth().currentUser.uid}/eventList`)
+      .child(eventId).on('value', snapshot => {
+        resolve({
+          id: snapshot.key,
+          name: snapshot.val().name,
+          date: snapshot.val().date,
+          price: snapshot.val().price,
+          cost: snapshot.val().cost,
+          revenue: snapshot.val().revenue
+        });
+      });
+    });
+  }
+
+  createEvent(eventName: string, eventDate: string, eventPrice: number, 
+    eventCost: number): firebase.Promise<any> {
+    return firebase.database().ref(`userProfile/${firebase.auth().currentUser.uid}/eventList`).push({
+      name: eventName,
+      date: eventDate,
+      price: eventPrice * 1,
+      cost: eventCost * 1,
+      revenue: eventCost * -1
+    });
+  }
+
+  addGuest(guestName, eventId, eventPrice, guestPicture = null): firebase.Promise<any> {
+    return firebase.database().ref(`userProfile/${firebase.auth().currentUser.uid}/eventList`)
+    .child(eventId).child('guestList').push({
+      guestName: guestName
+    }).then((newGuest) => {
+      firebase.database().ref(`userProfile/${firebase.auth().currentUser.uid}/eventList`)
+      .child(eventId).transaction( event => {
+        event.revenue += eventPrice;
+        return event;
+      });
+      if (guestPicture != null) {
+        firebase.storage().ref('/guestProfile/').child(newGuest.key)
+        .child('profilePicture.png').putString(guestPicture, 'base64', {contentType: 'image/png'})
+        .then((savedPicture) => {
+          firebase.database().ref(`userProfile/${firebase.auth().currentUser.uid}/eventList`)
+          .child(eventId).child('guestList').child(newGuest.key).child('profilePicture')
+          .set(savedPicture.downloadURL);
+        });        
+      }
+    });
+  }
+
+}
